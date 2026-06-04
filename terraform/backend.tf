19749a5c37bc92dd48e630f9e82fded2a2233461
nktrapp <ntrapp@furb.br>
2026-06-04 12:19:50 -0300

    nineteenth commit

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "furb-logistics-terraform-state"
-    key            = "package-service/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
